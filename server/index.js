defa95cb1b8662c2185096d3a67f61eeadda2d18
Kalid Ahmed <132435034+robustkode@users.noreply.github.com>
2023-12-29 17:04:32 +0300

    Update index.js
    cors

--- server/index.js
+++ server/index.js
@@ -27,7 +27,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
+const corsOptions = {
+  origin: 'http://example.com',
+  credentials: true
+};
+app.use(cors(corsOptions));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
